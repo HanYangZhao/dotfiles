d93582170bf87ca49a2f1cdd7b2c38bfc0cfd391
hyz <bobthesheep22@gmail.com>
2017-08-30 13:31:59 -0400

    added docker and sublime install

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,3 +23,16 @@ echo "**************************************************************************
 pip install powerline-status
 
 
+echo "***** INSTALL DOCKER*******"
+curl -o docker.sh https://get.docker.com/
+chmod +x docker.sh
+sudo ./docker.sh
+
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "**********INSTALL SUBLIME**************"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update && sudo apt install sublime-text
+
