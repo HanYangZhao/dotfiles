c9d8a129870e50d0d1133c29c4d0e8ee2d9968b0
han <han>
2020-05-18 18:48:10 +0000

    fix sublime install

--- bootstrap.sh
+++ bootstrap.sh
@@ -51,7 +51,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 echo "**********INSTALL SUBLIME**************"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update && sudo apt install sublime-text
+sudo apt update
+sudo apt install sublime-text
 
 echo "**************************************************************************"
 echo "************************Install Nodejs************************************"
